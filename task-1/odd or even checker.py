08d82fd859ee0b1aaef9cc0d764bdbc800babd97
syedmubashir9 <syedmubashirali0079@gmail.com>
2026-01-08 23:16:12 +0530

    Written program of even odd

--- task-1/odd or even checker.py
+++ task-1/odd or even checker.py
@@ -1,1 +1,15 @@
+while True:
+    try:
+        num = int(input("Enter an integer number: "))
+
+        if num == 0:
+            print("The number is Zero")
+        elif num % 2 == 0:
+            print("The number is Even")
+        else:
+            print("The number is Odd")
+        break  
+
+    except ValueError:
+        pass    
 
